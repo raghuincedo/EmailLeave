922aadfa8b1eef1daff12b2b122732b810ea03ee
ansh <incedohack2k18@gmail.com>
2018-02-13 16:13:50 +0530

    poller_updated

--- Poller/email_puller.py
+++ Poller/email_puller.py
@@ -2,47 +2,62 @@
 
 import imaplib
 import email
-import pickle
+import time
 
 # new mail generator --> yield after each mail to save resources
-def new_mail(last_uid, host, port, login, password):
-    # connect
-    mail_server = imaplib.IMAP4(host, port)
+class EMAILPOLLER():
+    def __init__(self,last_uid=None,host='mail.incedoinc.com', port=143,
+                 login='helpdesk_icf@incedoinc.com',password='feb@2018'):
 
-    # authenticate
-    mail_server.login(login, password)
-    mail_server.select('inbox')
+        self.last_uid=last_uid
+        self.host=host
+        self.port=port
+        self.password=password
+        self.login=login
+        self.sleep_time=2
 
-    # issue the search command of the form "SEARCH UID 42:*"
-    command = "UID {}:*".format(last_uid)
-    result, data = mail_server.uid('search', None, command)
-    messages = data[0].split()
-    #print(messages)
-    # yield mails
-    for message_uid in messages:
-        # SEARCH command *always* returns at least the most
-        # recent message, even if it has already been synced
-        if int(message_uid) > last_uid:
-            result, data = mail_server.uid('fetch', message_uid, '(RFC822)')
-            print('Message %s\n%s\n' % (int(message_uid), data[0][1]))
-            # yield raw mail body
+    def newMail(self):
+        mail_server = imaplib.IMAP4(self.host, self.port)
 
-            yield data[0][1]
+        mail_server.login(self.login, self.password)
+        mail_server.select('inbox')
+
+        if self.last_uid is None:
+            command='UNSEEN'
+        else:
+            command = "UID {}:*".format(self.last_uid)
+        result, data = mail_server.uid('search', None, command)
+        messages = data[0].split()
+
+        for message_uid in messages:
+            if self.last_uid is None or int(message_uid)>self.last_uid:
+                result, data = mail_server.uid('fetch', message_uid, '(RFC822)')
+                self.last_uid=int(message_uid)
+                # yield raw mail body
+
+                yield data[0][1]
+        mail_server.close()
+        mail_server.logout()
+
+    def pollerLoop(self):
+
+        while(1):
+            flag_1=0
+            for mail in email_poller.newMail():
+                email_message = email.message_from_bytes(mail)
+                print(email_message)
+            flag_1+=1
+
+            time.sleep(self.sleep_time)
+            if flag_1==0 and self.sleep_time<3:
+                self.sleep_time*=2
+            print(self.sleep_time)
 
 
-# usage example
-for mail in new_mail(last_uid=2,
-                               host='mail.incedoinc.com', port=143,
-                               login='helpdesk_icf@incedoinc.com',
-                               password='feb@2018'):
+email_poller=EMAILPOLLER()
+email_poller.pollerLoop()
 
-
-    with open('saved_email.pkl', 'wb') as output:
-        pickle.dump(mail, output, pickle.HIGHEST_PROTOCOL)
-
-    email_message = email.message_from_bytes(mail)
-    print(email_message)
-    #print(email_message[From])
+    '''
     if email_message.is_multipart():
         for part in email_message.get_payload():
             print(part.get_payload())
@@ -50,5 +65,4 @@ for mail in new_mail(last_uid=2,
         a=1
         print(email_message.get_payload(decode=True))
 
-    # do something useful with raw mail
-    pass
+    '''
