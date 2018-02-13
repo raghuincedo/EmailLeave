de6d1bdf1cd7b07e9a0179ee36516e8efc89f657
raghu <raghu.nandan@incedoinc.com>
2018-02-13 17:06:14 +0530

    data extractor code added

--- MainEngine/Email_processor.py
+++ MainEngine/Email_processor.py
@@ -1,15 +1,18 @@
 from MainEngine.Email_validator import EMAILVALIDITY
 from MainEngine.request_ID_exists import *
 from MainEngine.request_ID_not_exists import *
+from DbStorage import getter_setter
 
 class PROCESSINCOMINGEMAIL:
     def __init__(self):
         self.email_validity_checker = EMAILVALIDITY()
     def processMessage(self, original_email):
         ## get email_id
-        email_id = original_email
-        subject = original_email
-        valid = self.email_validity_checker.CheckSourceOfEmail(email_id)
+        requester_name = original_email.getRequesterName()
+        requester_email_id = original_email.getRequesterEmail()
+        requester_subject = original_email.getEmailSubject()
+        valid_source = self.email_validity_checker.CheckSourceOfEmail(requester_email_id)
+
         if valid:
             request_id_exist = self.email_validity_checker.CheckforRequestIdExistance(subject)
             if request_id_exist:
