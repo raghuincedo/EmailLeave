4a011d3546b28b455bcd42a97b36647f3c5b190d
ansh <incedohack2k18@gmail.com>
2018-02-16 08:33:21 +0530

    forward mail

--- Poller/Email_sender.py
+++ Poller/Email_sender.py
@@ -12,7 +12,8 @@ class EMAILSENDER:
         self.login=login
         self.password=password
 
-    def sendMail(self,to="user_icf@incedoinc.com",subject="test_mail",content="testing body"):
+    def sendMail(self,to="user_icf@incedoinc.com",subject="test_mail"
+                 ,content="testing body",reply_to=None):
 
         Msg = EmailMessage()
         Msg.set_content(content)
@@ -20,12 +21,14 @@ class EMAILSENDER:
         Msg['Subject'] = subject
         Msg['From'] = self.login
         Msg['To'] = to
-
+        if reply_to:
+            Msg.add_header("In-Reply-To",reply_to)
         Smtp_client = smtplib.SMTP(self.host)
         Smtp_client.login(self.login ,self.password)
         Smtp_client.send_message(Msg)
         Smtp_client.close()
 
 send_email=EMAILSENDER()
+reply_to='<24E10F677BCBF840AADB79B1C46F00F812811BA9@TS-GGN-MBX002.ibtechnology.com>'
 
-send_email.sendMail()
+send_email.sendMail(reply_to=reply_to)
